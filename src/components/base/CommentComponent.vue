0de55153497f2d102dde6fc00dfaeb359ed0b1a9
wj <wangjin@keenyoda.com>
2023-04-25 16:15:11 +0800

    feat: init评论组件

---
+++ src/components/base/CommentComponent.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="comment_body">
+    <a-comment>
+      <template #actions>
+        <span key="comment-nested-reply-to">Reply to</span>
+      </template>
+      <template #author>
+        <a>Han Solo</a>
+      </template>
+      <template #avatar>
+        <a-avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />
+      </template>
+      <template #content>
+        <p>
+          We supply a series of design principles, practical patterns and high quality design resources (Sketch and
+          Axure).
+        </p>
+      </template>
+      <a-comment>
+        <template #actions>
+          <span>Reply to</span>
+        </template>
+        <template #author>
+          <a>Han Solo</a>
+        </template>
+        <template #avatar>
+          <a-avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />
+        </template>
+        <template #content>
+          <p>
+            We supply a series of design principles, practical patterns and high quality design resources (Sketch and
+            Axure).
+          </p>
+        </template>
+        <a-comment>
+          <template #actions>
+            <span>Reply to</span>
+          </template>
+          <template #author>
+            <a>Han Solo</a>
+          </template>
+          <template #avatar>
+            <a-avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />
+          </template>
+          <template #content>
+            <p>
+              We supply a series of design principles, practical patterns and high quality design resources (Sketch and
+              Axure).
+            </p>
+          </template>
+        </a-comment>
+        <a-comment>
+          <template #actions>
+            <span>Reply to</span>
+          </template>
+          <template #author>
+            <a>Han Solo</a>
+          </template>
+          <template #avatar>
+            <a-avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />
+          </template>
+          <template #content>
+            <p>
+              We supply a series of design principles, practical patterns and high quality design resources (Sketch and
+              Axure).
+            </p>
+          </template>
+        </a-comment>
+      </a-comment>
+    </a-comment>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Comment } from 'ant-design-vue'
+export default defineComponent({
+  components: {
+    'a-comment': Comment
+  }
+})
+</script>
+<style scoped>
+.comment_body {
+  width:"";
+  background: hsla(0, 0%, 100%, 0.9);
+  border-radius: 10px;
+  -webkit-box-shadow: 0 0 12px rgba(0, 0, 0, 0.4);
+  box-shadow: 0 0 12px rgba(0, 0, 0, 0.4);
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,10 +4,12 @@
     <div class="article_list">
       <ArticleCard></ArticleCard>
     </div>
+    <CommentComponent></CommentComponent>
   </div>
 </template>
 
 <script lang="ts" setup>
 import NavigationBar from '@/components/base/NavigationBar.vue'
 import ArticleCard from '@/components/base/ArticleCard.vue';
+import CommentComponent from '@/components/base/CommentComponent.vue';
 </script>
